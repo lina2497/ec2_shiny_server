d1c58a7bb45865be0994f9790873a542fe100e95
Alex Betts <38247452+lina2497@users.noreply.github.com>
2021-02-14 08:31:01 +0000

    Update ec2_config.sh

--- ec2_config.sh
+++ ec2_config.sh
@@ -3,6 +3,16 @@ sudo yum -y update
 sudo amazon-linux-extras install R4 python3.8 postgresql11 vim golang1.11 ruby2.6 
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum-config-manager --enable epel
+
+# Increase virutal memory if using free tier with only 1GM ram
+# https://aws.amazon.com/premiumsupport/knowledge-center/ec2-memory-swap-file/
+sudo dd if=/dev/zero of=/swapfile bs=128M count=32
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon -s
+
+
 sudo yum install gedit libXcomposite libXcursor libXi libXtst libXrandr alsa-lib mesa-libEGL libXdamage mesa-libGL libXScrnSaver
 
 #install rstudio server
@@ -28,7 +38,7 @@ ldconfig
 #install r packages
 #installing sf in this way seems to fail, it works if you manually install in rstudio though.
 sudo Rscript -e 'install.packages("pacman", repos="https://cloud.r-project.org")'
-sudo Rscript -e 'pacman::p_load(shiny, tidyverse, leaflet, viridis, shinythemes, ggthemes, leaflet)'
+sudo Rscript -e 'pacman::p_load(shiny, tidyverse, leaflet, viridis, shinythemes, ggthemes, leaflet, DT)'
 
 #install rshiny server
 sudo wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.16.958-amd64.deb
