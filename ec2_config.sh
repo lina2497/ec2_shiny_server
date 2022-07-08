2ba80f33b3e574a9e42f1d5ca5389a0aca4749e8
Alex Betts <38247452+lina2497@users.noreply.github.com>
2022-07-08 19:04:14 +0100

    Update ec2_config.sh

--- ec2_config.sh
+++ ec2_config.sh
@@ -16,24 +16,24 @@ sudo swapon -s
 sudo yum install gedit libXcomposite libXcursor libXi libXtst libXrandr alsa-lib mesa-libEGL libXdamage mesa-libGL libXScrnSaver
 
 #install rstudio server
-sudo wget https://download2.rstudio.org/server/centos7/x86_64/rstudio-server-rhel-1.4.1103-x86_64.rpm
-sudo yum install rstudio-server-rhel-1.4.1103-x86_64.rpm
+wget https://download2.rstudio.org/server/centos7/x86_64/rstudio-server-rhel-2022.07.0-548-x86_64.rpm
+sudo yum install rstudio-server-rhel-2022.07.0-548-x86_64.rpm
 
 #install r package dependencies
 sudo yum install udunits2-devel openssl-devel libxml2-devel libcurl-devel 
 
 # install gdal (useful if you're using r package sf)
-sudo yum -y install make automake gcc gcc-c++ libcurl-devel proj-devel geos-devel proj-nad proj-epsg
-sudo yum-builddep gdal
-sudo wget http://download.osgeo.org/gdal/2.4.0/gdal-2.4.0.tar.gz
-sudo tar xf gdal-2.4.0.tar.gz
-cd gdal-2.4.0/
-./configure
-sudo make -j4
-sudo make install
+# sudo yum -y install make automake gcc gcc-c++ libcurl-devel proj-devel geos-devel proj-nad proj-epsg
+# sudo yum-builddep gdal
+# sudo wget http://download.osgeo.org/gdal/2.4.0/gdal-2.4.0.tar.gz
+# sudo tar xf gdal-2.4.0.tar.gz
+# cd gdal-2.4.0/
+# sudo ./configure
+# sudo make -j4
+# sudo make install
 # make sure the library can be found
-echo /usr/local/lib | tee -a /etc/ld.so.conf.d/local.conf
-ldconfig
+# echo /usr/local/lib | tee -a /etc/ld.so.conf.d/local.conf
+# ldconfig
 
 #install r packages
 #installing sf in this way seems to fail, it works if you manually install in rstudio though.
@@ -41,13 +41,13 @@ sudo Rscript -e 'install.packages("pacman", repos="https://cloud.r-project.org")
 sudo Rscript -e 'pacman::p_load(shiny, tidyverse, leaflet, viridis, shinythemes, ggthemes, leaflet, DT, shinycssloaders)'
 
 #install rshiny server
-sudo wget https://download3.rstudio.org/centos7/x86_64/shiny-server-1.5.16.958-x86_64.rpm
-sudo yum install --nogpgcheck shiny-server-1.5.16.958-x86_64.rpm
+wget https://download2.rstudio.org/server/centos7/x86_64/rstudio-server-rhel-2022.07.0-548-x86_64.rpm
+sudo yum install rstudio-server-rhel-2022.07.0-548-x86_64.rpm
 
 #add user for rstudio e.g. username: r_user
-sudo useradd r_user
+#sudo useradd r_user
 #set password for r_user
-sudo passwd r_user
+#sudo passwd r_user
 
 # Install git
 sudo yum install git 
